65e76fa82d61225619f5ced2f8135bdaf3c223f4
wongani18 <wonganinyirenda18@gmail.com>
2026-04-05 15:52:21 +0200

    feat: modified the products class

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
     @Get()
     findAll() {
-        return 'This action return all products'
+        return 'This action returns all products';
     }
+
+
+@Get(':id')
+findOne(@Param('id') id: string){
+    return 'This action returns product #${id}';
+}
 
 }
