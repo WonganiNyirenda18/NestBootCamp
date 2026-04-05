01846f991bf82bca9694057a271ea1b842659cfc
wongani18 <wonganinyirenda18@gmail.com>
2026-04-05 16:02:20 +0200

    feat: added the post request

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -7,10 +7,20 @@ export class ProductsController {
         return 'This action returns all products';
     }
 
-
-@Get(':id')
-findOne(@Param('id') id: string){
+    @Get(':id')
+    findOne(@Param('id') id: string){
     return 'This action returns product #${id}';
-}
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return 'This action creates a product';
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() body: any) {
+        return 'This action updates product #${id}';
+    }
+    
 
 }
