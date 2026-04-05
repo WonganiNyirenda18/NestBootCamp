884ccb49953320749e1e88e6491c3e7cefa58dcb
wongani18 <wonganinyirenda18@gmail.com>
2026-04-05 17:00:19 +0200

    feat: modified product services

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,4 +6,8 @@ export class ProductsService {
         {id: 1, name: 'Laptop', price: 999.99},
         {id: 2, name: 'Phone', price: 699.99},
     ];
+
+    findAll() {
+        return this.products
+    }
 }
