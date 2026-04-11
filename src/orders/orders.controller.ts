53e6cec35f2f2fc8abc3f01d658272037518a2e2
wongani18 <wonganinyirenda18@gmail.com>
2026-04-11 11:45:50 +0200

    feat: modified the order module

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private ordersService: OrdersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  createOrder(@Body() createOrderDto: CreateOrderDto, @Request() req) {
+    // req.user contains the authenticated user
+    return this.ordersService.create(createOrderDto, req.user.userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('my-orders')
+  getMyOrders(@Request() req) {
+    return this.ordersService.findByUser(req.user.userId);
+  }
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Order } from '../../orders/entities/order.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @OneToMany(() => Order, (order) => order.user)
+  orders: Order[];
+}
