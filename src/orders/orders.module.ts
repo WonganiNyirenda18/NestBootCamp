973ab0bd3538fa1c0ef96c128e7102d73f30325f
wongani18 <wonganinyirenda18@gmail.com>
2026-04-11 11:33:30 +0200

    modified orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ProductsService } from 'src/products/products.service';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService, ProductsService]
 })
 export class OrdersModule {}
