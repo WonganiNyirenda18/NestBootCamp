c303f495ed0bf863e4b70d310889bb9e68a2a43e
wongani18 <wonganinyirenda18@gmail.com>
2026-04-11 11:20:48 +0200

    modified the dto file

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,19 @@
+import { IsArray, IsInt, Min, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class OrderItemDto {
+  @IsInt()
+  @Min(1)
+  productId: number;
+
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}
+
+export class CreateOrderDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+}
