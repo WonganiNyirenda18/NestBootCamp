70dd5dbaee06d405c198c70e1fd2e060bc11e6ac
wongani18 <wonganinyirenda18@gmail.com>
2026-04-13 17:16:58 +0200

    feat: added a jwt key

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from 'src/users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'your_jwt_secret_key',
+      signOptions: {expiresIn: '1hr'},
+    }),],
   providers: [AuthService],
   controllers: [AuthController]
 })
